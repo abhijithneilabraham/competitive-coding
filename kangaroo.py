efce9bac8c74e11fdf0a9b493637535c988429ba
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-02-27 00:26:59 +0530

    Update kangaroo.py

---
+++ kangaroo.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 23:58:26 2019
+
+@author: abhijithneilabraham
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2):
+    for n in range(10000):
+        if((x1+v1)==(x2+v2)):
+            return "YES"
+        x1+=v1
+        x2+=v2
+    return "NO"
+                    
+               
+                
+                    
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
