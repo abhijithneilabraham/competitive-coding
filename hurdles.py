671ebdb9bc58947650e36fba3adbe55836633120
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-03 23:04:29 +0530

    submit

---
+++ hurdles.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 22:38:48 2019
+
+@author: abhijithneilabraham
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hurdleRace function below.
+def hurdleRace(k, height):
+    maximum=int(max(height))
+    potion=maximum-k
+    print(potion)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ temp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 22:46:17 2019
+
+@author: abhijithneilabraham
+"""
+
+print(max([1,6,6,5,2]))
