9eba20dc991a8a3d2ef8c612edeccefd552fd2d3
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-04-18 00:30:53 +0530

    Update maxsubsetsum.py

--- maxsubsetsum.py
+++ maxsubsetsum.py
@@ -7,25 +7,17 @@ Created on Wed Apr 17 23:58:44 2019
 """
 arr=[2,1,5,8,4]
 def maxSubsetSum(arr):
-    a=max(arr)
-    
-    b=arr.index(a)
-    '''
-    if b%2==0:
-        j=0
-        c=0
-        while j!='\0':
-            c+=arr[j]
-            j+=2
-    else:
-        k=1
-        d=0
-        while k!='\0':
+    c=0
+    d=0
+    for j in range(0,len(arr),2):
+                c+=arr[j]        
+    for k in range(1,len(arr),2):
             d+=arr[k]
-            k+=2
+        
     print(c,d)
-    '''
-    print(b)
 maxSubsetSum(arr)
+'''
+incomplete program
+'''
             
             
