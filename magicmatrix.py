401b3e328cb4c5a236b10c7f926dcf6e67eacfdc
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-05-05 01:53:58 +0530

    Update magicmatrix.py

--- magicmatrix.py
+++ magicmatrix.py
@@ -9,19 +9,25 @@ a=[[4,9,2],[3,5,7],[8,1,5]]
 def magicsquare(a):
     k=0
     b=0
+    c=0
     for i in range(3):
         k=sum(a[i])
-       
-        print(k)
+       # print(k)
+        if k!=c:
+            diff=abs(k-c)
+        print(diff)
+        c=k 
+        
     
     for j in range(3):
         l=0
         for m in range(3):
             l+=a[m][j]
-        print(l)
+       # print(l)
     for n in range(3):
          b+=a[n][n]
-    print(b)
+    #print(b)
+    
         
         
 magicsquare(a)
