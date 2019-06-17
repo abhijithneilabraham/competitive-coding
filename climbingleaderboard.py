7b37ad47e1016228bd5672b27cff67ad6ba426c3
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-18 03:48:07 +0530

    Update climbingleaderboard.py

--- climbingleaderboard.py
+++ climbingleaderboard.py
@@ -8,14 +8,15 @@ Created on Tue Jun 18 02:09:03 2019
 scores=[100, 90, 85, 80, 75, 60]
 
 alice=[0, 65, 77, 90, 102]
+
 def climbingLeaderboard(scores, alice):
+    scores=sorted(scores)
     rank=len(scores)
     count=0
     for i in range(len(alice)):
-        for j in scores:
-            if alice[i]>j:
-            
-                rank=rank-1
+        for j in (len(scores)-1,0):
+            if alice[i]>scores[j]:
+                rank=len(scores)-1-j
                 break
         print(rank)
 climbingLeaderboard(scores,alice)
