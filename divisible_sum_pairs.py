29978971970664021bbd064ff481e1883f067a5f
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-22 01:56:58 +0530

    Update divisible_sum_pairs.py

--- divisible_sum_pairs.py
+++ divisible_sum_pairs.py
@@ -5,17 +5,25 @@ Created on Sat Jun 22 01:18:30 2019
 
 @author: abhijithneilabraham
 """
-arr=[1,3,2,6,1,2]
-n=6
-k=3
+arr=[5,9,10,7,4]
+
+n=5
+k=2
 def divisibleSumPairs(n, k, arr):
     count=0
+    '''
     for i in range(len(arr)):
-        n=arr[i]%k
-        if n!=0 :
-            for j in range(len(arr)):
-                if n==j and j!=i:
-                    count=count+1
+        m=arr[i]%k
+        
+        for j in range(len(arr)):
+            if arr[j]==m and j!=i :
+                count=count+1
+    return count
+    '''
+    for i in range(n):
+      for j in range(i+1,n):
+          if(arr[i]+arr[j])%k==0:
+              count=count+1
     return count
                     
 print(divisibleSumPairs(n,k,arr))
