719d6734967bdb69a686996be0646f69084a4503
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-21 23:23:31 +0530

    Update pagecount.py

--- pagecount.py
+++ pagecount.py
@@ -7,6 +7,26 @@ Created on Fri Jun 21 22:46:30 2019
 """
 
 def pageCount(n, p):
-    for i in range(2,n,2):
-        print(i)
-print(pageCount(100,4))
+    count=0
+    if n%2==0 and n>p:
+    
+        for i in range(n,p,2):
+            count+=1
+    elif n%2==0 and n<p:
+        for i in range(p,n,-2):
+            count+=1
+    elif n%2!=0 and n>p:
+        n-=1
+        for i in range(n,p,2):
+            count+=1
+    elif n%2!=0 and n<p:
+        n=n-1
+        for i in range(n,p,-2):
+            count=count+1
+    elif n==p:
+        count =0
+    return count-1
+    
+n=int(input())
+p=int(input())
+print(pageCount(p,n))
