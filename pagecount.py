7d35128e840b31cace6338219b2135337493cbff
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-21 23:06:17 +0530

    Update pagecount.py

---
+++ pagecount.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 21 22:46:30 2019
+
+@author: abhijithneilabraham
+"""
+
+def pageCount(n, p):
+    for i in range(2,n,2):
+        print(i)
+print(pageCount(100,4))
