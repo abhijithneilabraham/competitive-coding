37591f404483c3ea1cb2f22f4edfbb1123729592
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-22 19:37:02 +0530

    Update migratorybirds.py

--- migratorybirds.py
+++ migratorybirds.py
@@ -5,12 +5,25 @@ Created on Sat Jun 22 15:05:25 2019
 
 @author: abhijithneilabraham
 """
-arr=[1,4,4,4,5,3]
+arr=[6,6,6,4,6,3]
 def migratoryBirds(arr):
-    a=[0]*len(arr)
-    for i in range(len(arr)):
-        a[arr[i]] += 1
-    print( a.index(max(a)))
+ 
+    freq={}
+    for i in arr:
+        if i in freq:
+            freq[i]+=1
+            
+        else:
+            freq[i]=1
+           
+    print(max(freq))
+    for key,values in freq.items():
+        
+        if values==max(freq):
+            
+            print(key)
+            return key
+            
         
         
                 
