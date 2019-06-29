ec2b0b23328b03e45ca7d0bce4847656f250a03c
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-29 16:30:01 +0530

    Update Sock-Merchant.py

--- Sock-Merchant.py
+++ Sock-Merchant.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb  5 20:20:07 2019
+Created on Sat Jun 29 16:19:59 2019
 
 @author: abhijithneilabraham
 """
-
+arr=[10,20,20,10,10,30,50,10,20]
+def sockMerchant(n, ar):
+    ar=list(sorted(ar))
+    print(ar)
+    i=0
+    count=0
+    while(i<n-1):
+        i+=1
+        if ar[i]==ar[i-1]:
+            count+=1
+            i+=1
+    print(count)
+    return count
+sockMerchant(9,arr)
