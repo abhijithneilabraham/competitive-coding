2456e160b2cc40458e833a56473d64740a145b76
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-07-03 15:58:56 +0530

    Update codesprint/isfiboornot.py

--- codesprint/isfiboornot.py
+++ codesprint/isfiboornot.py
@@ -5,4 +5,23 @@ Created on Wed Jul  3 15:22:28 2019
 
 @author: abhijithneilabraham
 """
+def solve(i):
+    def fib(n):
+        m=1
+     
+        if n<=1:
+           
+            return m
+        else:
+             m=fib(n-1)+fib(n-2)
+           
+             return m
+    flag='IsNotFibo'
+    for j in range(i):
+        if i==fib(j):
+            flag='IsFibo'
+            
+    return flag
+print(solve(10))
 
+             
