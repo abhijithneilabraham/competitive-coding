ec5997fc6058b6b7693d848e7c786fecc1374455
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-08-16 22:15:14 +0530

    chaotic half

---
+++ arrays/chaotic.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 14:27:25 2019
+
+@author: abhijithneilabraham
+"""
+a=[1,2,5,3,7,8,6,4]
+
+def minimumBribes(a):
+    b=sorted(a)
+    print(b)
+    count=0
+    c=[]
+    i=0
+    while(i<len(a)-1):
+        if a[i]-b[i] and a[i]-b[i+1]  >2:
+            print("Too Chaotic")
+        else:
+            if a[i]!=b[i]:
+                if a.index(5)+1>b[i]:
+                    count+=a[i]-b[i]
+                
+                
+        i+=1
+    print(count)
+            
+minimumBribes(a)
+    
---
+++ arrays/rotateleft.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int rotleft(int arr2[],int d);
+int main(){
+  int arr[]={1,2,3,4,5,6};
+  rotleft(arr,3);
+
+}
+int rotleft(int arr2[],int d){
+  int size=arr2[0].length();
+  cout<<"the siz si"<<size;
+}
