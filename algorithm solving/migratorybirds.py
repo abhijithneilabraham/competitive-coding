fc4840f1f1e11b884ead3f63d08c68bfc97df668
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-08-18 10:54:04 +0530

    Update algorithm solving/migratorybirds.py

--- algorithm solving/migratorybirds.py
+++ algorithm solving/migratorybirds.py
@@ -1,23 +1,15 @@
-arr=[6,6,6,4,4,4,6,4,4,4]
+arr=[1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
 def migratoryBirds(arr):
-    count=1
-    maxd=1
-    b=arr[0]
-    a=sorted(arr)
+    count=[0]*len(arr)
+    b=sorted(arr)
     for i in range(len(arr)):
-        if arr[i-1]==arr[i]:
-            count+=1
-        else:
-            if count>maxd:
-                maxd=count
-                b=arr[i-1]            
-            count=1
-        if count>maxd:
-            maxd=count
-            b=arr[len(arr)-1]
-        
-        print(b)
-        return b
+        if b[i-1]==b[i]:
+            count[i]+=count[i-1]+1
+    
+    print(b[count.index(max(count))])
+            
+    
+
     
                 
         
