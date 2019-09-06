5f0c082b3c48b9d2ff92aa2ab77cfd873671eaf7
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-09-07 02:56:54 +0530

    Update algorithm solving/hurdelrace.py

---
+++ algorithm solving/hurdelrace.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 02:52:59 2019
+
+@author: abhijithneilabraham
+"""
+ht=[1,6,3,5,2]
+def hurdleRace(k, height):
+    m=max(height)
+    if k>=m:
+        print(0)
+        return 0
+    else:
+        print(m-k)
+        return m-k
+hurdleRace(4,ht)
+        
