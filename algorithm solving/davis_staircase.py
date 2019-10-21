530ba43ac1df0759d951e2d0396f7ce830373924
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-10-21 14:38:23 +0530

    Update algorithm solving/davis_staircase.py

--- algorithm solving/davis_staircase.py
+++ algorithm solving/davis_staircase.py
@@ -5,12 +5,16 @@ Created on Mon Oct 21 14:23:20 2019
 
 @author: abhijithneilabraham
 """
-
+import datetime
+start=datetime.datetime.now()
 def stepPerms(n): 
+    a=stepPerms
     if n==0 or n==1:
         return 1
     elif n==2:
         return n
     else:
-        return stepPerms(n-3)+stepPerms(n-2)+stepPerms(n-1)
+        return a(n-3)+a(n-2)+a(n-1)
+stop=datetime.datetime.now()
 print(stepPerms(10))
+print(stop-start)
