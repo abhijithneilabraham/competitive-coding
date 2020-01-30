4ace31c508e78906b58770f550e2d11e447d251f
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-01-30 22:32:58 +0530

    works

--- project_euler/prime.cpp
+++ project_euler/prime.cpp
@@ -1,25 +1,27 @@
 #include<iostream>
 using namespace std;
 int main(){
-     const long n=24;
-     long z=n;
-     long fact=0;
+     const long numm=600851475143;
+     long z=numm;
+     long lfact=numm;
     int p=2;
-    while(p*p<=z){
+    while(p*p <= z){
         if(z%p==0){
-            z/=p;
-            fact=p; 
-
+            z=z/p;
+            lfact=p; 
         }
         else{
             p++;
         }           
 
         }
+        if(z>lfact){
+            lfact=z;
+        }
 
 
     
  
-cout<<"The output is="<<fact;
+cout<<"The output is="<<lfact;
 return 0;
 }
