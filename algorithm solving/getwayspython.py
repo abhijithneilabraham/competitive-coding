7d102bb0f915c25e0f5d39f08360e534d3a8baf4
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-03-03 11:05:34 +0530

    Update algorithm solving/getwayspython.py

--- algorithm solving/getwayspython.py
+++ algorithm solving/getwayspython.py
@@ -1,36 +1,23 @@
-def getWays(n, c):
-    k=0
-    c=sorted(c)
-    i=0
-    
-getWays(10,[2,5,3,6])
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 00:20:23 2020
 
-# Change problem 
-def count(S, m, n): 
-  
-    # table[i] will be storing the number of solutions for 
-    # value i. We need n+1 rows as the table is constructed 
-    # in bottom up manner using the base case (n = 0) 
-    # Initialize all table values as 0 
-    table = [0 for k in range(n+1)] 
-  
-    # Base case (If given value is 0) 
-    table[0] = 1
-  
-    # Pick all coins one by one and update the table[] values 
-    # after the index greater than or equal to the value of the 
-    # picked coin 
-    for i in range(0,m): 
-        for j in range(S[i],n+1): 
-            table[j] += table[j-S[i]] 
-            print(table[j-S[i]])
-        print("next itu")
-    return table[n] 
-  
-# Driver program to test above function 
-arr = [2, 5, 3,6] 
-m = len(arr) 
-n = 10
-x = count(arr, m, n) 
-print (x) 
-  
+@author: abhijithneilabraham
+"""
+
+l=0
+
+def getWays(n, c):
+    global l
+    if n==0:
+        return 0
+    for i in range(len(c)):
+        if c[i]<=n:
+            l+=1
+            getWays(n-c[i],c)
+            
+            return l
+            
+            
+print(getWays(4,[1,2,3]))
