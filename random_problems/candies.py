66cc62a1ddeea527c2df05429e3597bd95f2ca8e
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-03-31 00:49:05 +0530

    aight typos....

--- random_problems/candies.py
+++ random_problems/candies.py
@@ -6,6 +6,37 @@ Created on Mon Mar 30 20:44:42 2020
 @author: abhijithneilabraham
 """
 a=[2,4,2,6,1,7,8,9,2,1]
+f=open('testcasecandies.txt','r')
+x=f.readlines()
+y=[]
+
+
+for i in range(len(x)):
+    y.append(x[i])
+
+def candies(n, arr):
+    candy_lr = [1]*(len(arr))
+    candy_rl = [1]*(len(arr))
+    candy = []
+    
+    # traverses from left to right and assigns candy to students
+    for i in range(1,len(arr)):
+        if arr[i]>arr[i-1]:
+            candy_lr[i] = candy_lr[i-1]+1
+    
+    # traverses from right to left and assigns candy to students        
+    for i in range(len(arr)-2,-1,-1):
+        if arr[i]>arr[i+1]:
+            candy_rl[i] = candy_rl[i+1]+1   
+    
+    #calculates the total candy needed
+    for i in range(0,len(arr)):
+        candy.append(max(candy_lr[i],candy_rl[i]))
+    print(sum(candy))
+    return sum(candy)
+        
+        
+            
 def candies(n, arr):
     cl=[1]*n
     cr=[1]*n
@@ -23,10 +54,6 @@ def candies(n, arr):
         s+=max(cl[i],cr[i])
     print(s)
     return s
-        
-        
-            
-            
     
         
             
@@ -36,5 +63,5 @@ def candies(n, arr):
         
         
     
-print(candies(len(a),a))
+print(candies(len(y),y))
     
