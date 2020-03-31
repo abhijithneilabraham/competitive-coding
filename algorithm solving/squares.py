87f9d35ea6348f02ffe71b54e10291a6577ce7de
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-04-01 04:13:24 +0530

    Update algorithm solving/squares.py

--- algorithm solving/squares.py
+++ algorithm solving/squares.py
@@ -6,9 +6,8 @@ Created on Wed Apr  1 03:51:28 2020
 @author: abhijithneilabraham
 """
 def squares(a, b):
-    rt1=int(a**(1/2))
-    rt2=int(b**(1/2))
-    a=[i**2 for i in range(rt1,rt2+2) if i**2>=a and i**2<=b]
-    return len(a)
+
+    a=len([i**2 for i in range(int(a**(1/2)),int(b**(1/2))+2) if i**2>=a and i**2<=b])
+    return a
 print(squares(3,9))
             
