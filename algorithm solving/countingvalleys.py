190e0cf91cd5257e357442fdb0948806bde313aa
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-11 23:35:14 +0530

    Update algorithm solving/countingvalleys.py

--- algorithm solving/countingvalleys.py
+++ algorithm solving/countingvalleys.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun 29 17:15:18 2019
+Created on Fri Sep 11 23:33:32 2020
 
 @author: abhijithneilabraham
 """
-arr='UDDDUDUU'
 
 def countingValleys(n, s):
-    level=0
-    valley=0
-    for i in range(n):
-        if s[i]=='U':
-            level+=1
-        elif s[i]=='D':
-            level-=1
-            
-        if level<0 and s[i] =='U' :
-            valley+=1
-    return valley        
-print(countingValleys(8,arr))
+    c=0
+    d=0
+    for i in range(len(s)):
+        if s[i] in 'U':
+            c+=1
+        else:
+            c-=1
+        if c==-1 and s[i] not in 'U':
+            d+=1
+    return d
+    
