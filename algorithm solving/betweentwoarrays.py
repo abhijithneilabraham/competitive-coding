f074860f9966a4d5e5d93ef9258bc9e3b2ee79d6
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-12-10 03:05:36 +0530

    Update algorithm solving/betweentwoarrays.py

--- algorithm solving/betweentwoarrays.py
+++ algorithm solving/betweentwoarrays.py
@@ -1,31 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 22 23:12:13 2019
+import math
 
-@author: abhijithneilabraham
-"""
-a=[3,9,6]
-b=[36,72]
+# function to calculate LCM
+def lcmarr(a):
+  lcm = a[0]
+  for i in range(1,len(a)):
+    lcm = lcm*a[i]//math.gcd(lcm, a[i])
+  return lcm
+
+def gcdarr(arr):
+    b=arr[0]
+    for j in range(1,len(arr)):
+        s=math.gcd(b,arr[j])
+        b=s
+    return b
+
 def getTotalX(a, b):
-    c=max(b)
-    flag=0
-    count=0
-    for j in range(c):
-        for i in a:
-            if j%i==0:
-                flag=1
-            else:
-                flag==0
-                break
-        if flag==1 :
-            count+=1
-    print(count)
-            
-        
-            
-            
-getTotalX(a,b)
-                
-            
+    lcma=lcmarr((a))
+    gcdb=gcdarr(b)
+    res=[]
+    for i in range(lcma,gcdb+1,lcma):
+        if gcdb%i==0:
+            res.append(i)
+    return res
     
